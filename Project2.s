44eeebe00f9261c49b12d5ff6f9fa0f4cceeb6d8
zachslone <zslone1@umbc.edu>
2026-04-26 23:54:33 -0400

    Update Project2.s

--- Project2.s
+++ Project2.s
@@ -1,4 +1,34 @@
 .section .data
   prompt_msg:  .ascii "The double is: "
   prompt_len:  .quad . - prompt_msg
-  newline:
+  newline:     .ascii "\n"
+
+.section .bss
+  .lcomm buffer, 16
+  .lcomm out_buf, 16
+
+.section . text
+  .globl _start
+
+_start:
+  mov $0, %rax
+  mov $0, %rdi
+  mov $buffer, %rsi
+  mov $16, %rdx
+  syscall
+
+  movzbq (buffer), %rax
+  sub $48, %rax
+
+  add %rax, %rax
+
+  mov $10, %rcx
+  mov $out_buf, %rdi
+  add $15, %rdi
+  movb $10, (%rdi)
+
+convert_loop:
+  dec %rdi
+  xor %rdx, %rdx
+  div %rcx
+  add $48, %dl
